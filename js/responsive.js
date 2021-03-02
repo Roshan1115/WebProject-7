d1e648b12108abc17181685b1c9487d99e2622bf
Roshan1115 <roshanbishi7890@gamil.com>
2021-03-02 13:50:01 +0530

    Responsive beta 2

--- js/responsive.js
+++ js/responsive.js
@@ -2,6 +2,11 @@ navbar = document.querySelector('.navbar')
 navlist = document.querySelector('.navlist')
 navright = document.querySelector('.rightnav')
 tap_view = document.querySelector('.tap-to-view')
+linkTouch1 = document.querySelector('#link-to-touch1')
+linkTouch2 = document.querySelector('#link-to-touch2')
+linkTouch3 = document.querySelector('#link-to-touch3')
+linkTouch4 = document.querySelector('#link-to-touch4')
+searchbtn = document.querySelector('.btn-small')
 
 // tap_view.addEventListener('click', () => {
 //     navbar.classList.toggle('res-navbar')
@@ -10,4 +15,30 @@ tap_view.addEventListener('click', () => {
     navbar.classList.toggle('res-navbar')
     navlist.classList.toggle('res-navlist')
     navright.classList.toggle('res-navlist')
+})
+
+linkTouch1.addEventListener('click', () => {
+    navbar.classList.toggle('res-navbar')
+    navlist.classList.toggle('res-navlist')
+    navright.classList.toggle('res-navlist')
+})
+linkTouch2.addEventListener('click', () => {
+    navbar.classList.toggle('res-navbar')
+    navlist.classList.toggle('res-navlist')
+    navright.classList.toggle('res-navlist')
+})
+linkTouch3.addEventListener('click', () => {
+    navbar.classList.toggle('res-navbar')
+    navlist.classList.toggle('res-navlist')
+    navright.classList.toggle('res-navlist')
+    alert('Services section is not available yet. Thank you for your understanding.')
+})
+linkTouch4.addEventListener('click', () => {
+    navbar.classList.toggle('res-navbar')
+    navlist.classList.toggle('res-navlist')
+    navright.classList.toggle('res-navlist')
+})
+
+searchbtn.addEventListener('click', () => {
+    alert('The search function is not available yet. Thank you for your understanding.')
 })
